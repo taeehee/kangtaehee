66eb34648e0262e3805dfbda3c15ca0337bdb06f
taeehee <gmlzlxl2013@gmail.com>
2025-11-10 14:31:10 +0900

    section 모션 안겹치게 정리(main_section안에 section들 다 넣음)

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(function () {
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".letter_section",
-      start: "top top",
-      end: "bottom 0%",
+      start: "top 200px", /* 헤더높이부터 편지위치 고정 */
+      /* end: "bottom 0%", 초기값*/
+      end: () => `+=${window.innerHeight * 4}px`,
+      /* 뷰포트 높이의 4배만큼 스크롤했을 때 */
       scrub: true,
       pin: true,
       markers: true
@@ -15,11 +17,13 @@ $(function () {
   });
 
   tl.fromTo(".envelope-closed", { opacity: 1 }, { opacity: 0, duration: 0.5 })
-    .fromTo(".envelope-flap", {  rotationX: 0, transformOrigin: "top center" }, {  rotationX: -180, duration: 2, ease: "power2.inOut", zIndex: 1 })
-    .fromTo(".letter", { transformOrigin: "top 90%", opacity: 1}, { y: -210, opacity: 1, duration: 2, ease: "power2.out" });
+    .fromTo(".envelope-body", {  opacity: 1 }, { zIndex: 4 })
+    .fromTo(".envelope-flap", {  rotationX: 0, transformOrigin: "top center",  zIndex: 4 }, {  rotationX: -180, duration: 1, ease: "power2.inOut", zIndex: 0}, 0.2)/* 0.2초 후 시작 */
+    .fromTo(".letter", { transformOrigin: "top end", opacity: 1, zIndex: -1}, { y: -210, opacity: 1, duration: 2, /* ease: "power2.inOut",  */zIndex: 3 });
   // 편지 애니메이션end (타임라인1)
 
 
+// text 애니메이션
   gsap.registerPlugin(ScrollTrigger);
 let revealAnimations = [];
 
@@ -33,7 +37,7 @@ gsap.ticker.add((time)=>{
   lenis.raf(time * 1000)
 })
 
-// Reveal
+// reveal
 document.querySelectorAll('.reveal').forEach(text => {
   // Split text
   let splitText = new SplitType(text, {
@@ -51,9 +55,11 @@ document.querySelectorAll('.reveal').forEach(text => {
       trigger: ".container",
       start: 'top top', 
       end: () => `+=${window.innerHeight * 5}px`,
+      /* 초기값: 뷰포트 높이의 5배만큼 스크롤했을 때 */
       scrub: true,
       // markers: true,
       pin: true,
+      markers: true
     }
   })
 })
